0ac78429897417abe01bc0b84731fa2f14017bd5
mayank241995 <srivastava.mayank241995@gmail.com>
2025-11-28 23:52:03 +0530

    add record to table

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -18,7 +18,31 @@ namespace DataAccess.Data
         }
         protected override void OnModelCreating (ModelBuilder modelBuilder)
         {
+            // setting precision for Price property in Book entity
             modelBuilder.Entity<Book>().Property(b => b.Price).HasPrecision(10,5);
+            //creating entry
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    IDBook = 1,
+                    Title = "Sample Book",
+                    ISBN = "123-4567890123",
+                    Price = 19.99000m
+                },
+               new Book {
+                   IDBook = 2,
+                   Title = "Another Book",
+                   ISBN = "987-6543210987",
+                   Price = 29.99500m
+               }
+            );
+            var bookList = new Book[]
+            {
+                new Book{IDBook=3,Title="C# Programming",ISBN="111-2223334445",Price=39.99000m},
+                new Book{IDBook=4,Title="ASP.NET Core Guide",ISBN="555-6667778889",Price=49.99500m},
+                new Book{IDBook=5,Title="Entity Framework Core",ISBN="999-0001112223",Price=59.99000m}
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
         }
     }
 }
---
+++ DataAccess/Migrations/20251128164214_seedBookTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "IDBook", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123-4567890123", 19.99000m, "Sample Book" },
+                    { 2, "987-6543210987", 29.99500m, "Another Book" },
+                    { 3, "111-2223334445", 39.99000m, "C# Programming" },
+                    { 4, "555-6667778889", 49.99500m, "ASP.NET Core Guide" },
+                    { 5, "999-0001112223", 59.99000m, "Entity Framework Core" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 5);
+        }
+    }
+}
