2c313ba5d24b1dba72e10f9469e79ad968c6acd5
mayank241995 <srivastava.mayank241995@gmail.com>
2025-11-29 00:28:56 +0530

    add notmapeed in book model

--- Model/Models/Book.cs
+++ Model/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,12 @@ namespace Model.Models
         [Key]
         public int IDBook { get; set; }
         public string? Title { get; set; }
-        public string? ISBN { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string ISBN { get; set; }
         public decimal Price { get; set; }
+        [NotMapped]
+        public string PriceRange { get; set; }
 
     }
 }
