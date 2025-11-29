a1fd2192962ec96e5efd1e6bdebeae92ba556291
mayank241995 <srivastava.mayank241995@gmail.com>
2025-11-29 18:25:02 +0530

    create a new table author publisher and subcategory

---
+++ Model/Models/SubCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int SubCategory_Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Required { get; set; }
+    }
+}
